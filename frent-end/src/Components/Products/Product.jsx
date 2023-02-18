9741ddf7bb177007f38486d673557e4045dda029
mohamed-ilyass-elghazi <mohamed-ilyass-elghazi>
2023-02-18 12:34:37 +0100

    products page is completed

--- frent-end/src/Components/Products/Product.jsx
+++ frent-end/src/Components/Products/Product.jsx
@@ -8,29 +8,26 @@ import style from './Products.module.css'
 export default function Product() {
   return (
     <div className={style.product}>
-        <div>
+        <div className={style.imagezone}>
             <img src={require('../Images/home.jpg')} alt="" />
         </div>
-        <div>
-            <div>
+        <div className={style.productInfo}>
+            <div className={style.productName}>
                 <p>product name </p>
                 <div>
-                <Checkbox  icon={<FavoriteBorder />} checkedIcon={<Favorite />} />
+                <Checkbox className={style.icon} color="error"icon={<FavoriteBorder />} checkedIcon={<Favorite />} />
                 <Checkbox
-                
+                color="error"
                 icon={<BookmarkBorderIcon />}
                 checkedIcon={<BookmarkIcon />}
                 />
                 </div>
             </div>
-            <p>category</p>
-            <Rating name="read-only" value="2" readOnly />
-            <p>40Dh</p>
-            <ul>
-                <li>Lorem ipsum dolor sit.</li>
-                <li>Numquam exercitationem dolorum illum.</li>
-                <li>Quae voluptatem fugiat vel.</li>
-            </ul>
+            <p className={style.category}>category</p>
+            <Rating name="read-only" className={style.icon} value="2" readOnly />
+            <p className={style.price}>40Dh</p>
+            <p className={style.description}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi fugiat dignissimos 
+                libero nihil repudiandae architecto aliquid tempore ipsa nemo accusantium!</p>
         </div>
     </div>
   )
--- frent-end/src/Components/Products/Products.jsx
+++ frent-end/src/Components/Products/Products.jsx
@@ -7,9 +7,24 @@ import { MdTune } from 'react-icons/md';
 import Select from '@mui/material/Select';
 import MenuItem from '@mui/material/MenuItem';
 import Product from './Product';
+import { useTheme } from '@mui/material/styles';
+import MobileStepper from '@mui/material/MobileStepper';
+import Button from '@mui/material/Button';
+import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
 
 
 export default function Products() {
+    const theme = useTheme();
+  const [activeStep, setActiveStep] = React.useState(0);
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
   return (
     <React.Fragment     >
         <div className={style.parent}>
@@ -59,7 +74,7 @@ export default function Products() {
                             labelId="demo-select-small"
                             id="demo-select-small"
                             // value={age}
-                            label="Age"
+                            label="Sort By"
                             // onChange={handleChange}
                             color='secondary'
                         >
@@ -72,7 +87,48 @@ export default function Products() {
                         </Select>
                     </FormControl>
                     </div>
-                    <Product />
+                    <div className={style.products}>
+                        <Product />
+                        <Product />
+                        <Product />
+                        <Product />
+                        <Product />
+                        <Product />
+                        <Product />
+                        <Product />
+                        <Product />
+                        <Product />
+                        <Product />
+                        <MobileStepper
+      variant="dots"
+      steps={6}
+      color ="secondary"
+      position="static"
+      activeStep={activeStep}
+      sx={{ maxWidth: 1000, flexGrow: 1 }}
+      nextButton={
+        <Button size="small" onClick={handleNext} color ="secondary" disabled={activeStep === 5}>
+          Next
+          {theme.direction === 'rtl' ? (
+            <KeyboardArrowLeft />
+          ) : (
+            <KeyboardArrowRight />
+          )}
+        </Button>
+      }
+      backButton={
+        <Button size="small" onClick={handleBack} color ="secondary" disabled={activeStep === 0}>
+          {theme.direction === 'rtl' ? (
+            <KeyboardArrowRight />
+          ) : (
+            <KeyboardArrowLeft />
+          )}
+          Back
+        </Button>
+      }
+    />
+                    </div>
+                    
                 </div>
                
                
