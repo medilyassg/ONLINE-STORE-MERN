a4ee3d4a7731248f6dc3965242acd61aff04402c
mohamed-ilyass-elghazi <mohamed-ilyass-elghazi>
2023-02-27 17:53:14 +0100

    products list

--- frent-end/src/Components/AdminDashbord/Products/Productslists.jsx
+++ frent-end/src/Components/AdminDashbord/Products/Productslists.jsx
@@ -4,7 +4,9 @@ import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 import style from './Products.module.css'
-
+import { IconButton } from '@mui/material';
+import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
+import ModeEditIcon from '@mui/icons-material/ModeEdit';
 export default function Productslist() {
   const [age, setAge] = React.useState('');
 
@@ -76,6 +78,66 @@ export default function Productslist() {
 
     </FormControl>
 
+      </div>
+      <div className={style.list}>
+        <div className={style.header}>
+          <p className={style.name}>Product Name </p>
+          <p className={style.category}>Category</p>
+          <p className={style.price}>Price</p>
+          <p className={style.inventory}>Inventory</p>
+          <p className={style.date}>Date Added</p>
+
+        </div>
+        <div className={style.items}>
+          <div className={style.item}>
+            <p className={style.name}>product 1</p>
+            <p className={style.category}>food</p>
+            <p className={style.price}>45 $</p>
+            <p className={style.inventory}>45</p>
+            <p className={style.date}>2018/01/23</p>
+            <div>
+            <IconButton variant="soft" color='error'>
+              <DeleteOutlineIcon  />
+            </IconButton>
+            <IconButton variant="soft" color='success'>
+              <ModeEditIcon  />
+            </IconButton>
+            </div>
+            
+          </div>
+          <div className={style.item}>
+            <p className={style.name}>product 1</p>
+            <p className={style.category}>food</p>
+            <p className={style.price}>45 $</p>
+            <p className={style.inventory}>45</p>
+            <p className={style.date}>2018/01/23</p>
+            <div>
+            <IconButton variant="soft" color='error'>
+              <DeleteOutlineIcon  />
+            </IconButton>
+            <IconButton variant="soft" color='success'>
+              <ModeEditIcon  />
+            </IconButton>
+            </div>
+            
+          </div>
+          <div className={style.item}>
+            <p className={style.name}>product 1</p>
+            <p className={style.category}>food</p>
+            <p className={style.price}>45 $</p>
+            <p className={style.inventory}>45</p>
+            <p className={style.date}>2018/01/23</p>
+            <div>
+            <IconButton variant="soft" color='error'>
+              <DeleteOutlineIcon  />
+            </IconButton>
+            <IconButton variant="soft" color='success'>
+              <ModeEditIcon  />
+            </IconButton>
+            </div>
+            
+          </div>
+        </div>
       </div>
     </div>
   )
